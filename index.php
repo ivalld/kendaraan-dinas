fd7c0afc598c57b745e823624f0905b25bbc40db
ivalld <rnoviar21@gmail.com>
2023-12-15 11:22:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,147 @@
+<?php
+session_start();
+include('includes/config.php');
+// include('includes/format_rupiah.php');
+error_reporting(0);
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <title>Vehicle Management System Kolinlamil</title>
+  <!--Bootstrap -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+  <link href="assets/css/slick.css" rel="stylesheet">
+  <link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+  <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+  <link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+</head>
+
+<body>
+
+  <!--Header-->
+  <?php include('includes/header.php'); ?>
+  <!-- /Header -->
+
+  <!-- Banners -->
+  <section id="banner" class="banner-section">
+    <div class="container">
+      <div class="div_zindex">
+        <div class="row">
+          <div class="col-md-5 col-md-push-7">
+            <div class="banner_content">
+              <h1>Vehicle Management System</h1>
+              <p>Dinas Kolinlamil . </p>
+              <!-- <a href="car-listing.php" class="btn">Selengkapnya <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /Banners -->
+
+
+  <!-- Resent Cat-->
+  <section class="section-padding gray-bg">
+    <div class="container">
+      <div class="row">
+
+        <!-- Nav tabs -->
+        <div class="recent-tab">
+          <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" class="active"><a href="#resentnewcar" role="tab" data-toggle="tab">Kendaraan Dinas Kolinlamil</a></li>
+          </ul>
+        </div>
+        <!-- Recently Listed New Cars -->
+        <div class="tab-content">
+          <div role="tabpanel" class="tab-pane active" id="resentnewcar">
+
+            <?php
+            $sql = "SELECT mobil.*, merek.* FROM mobil, merek WHERE merek.id_merek = mobil.id_merek";
+            $query = mysqli_query($koneksidb, $sql);
+            if (mysqli_num_rows($query) > 0) {
+              while ($results = mysqli_fetch_array($query)) {
+
+            ?>
+
+                <div class="col-list-3">
+                  <div class="recent-car-list">
+                    <div class="car-info-box"> <a href="vehical-details.php?vhid=<?php echo htmlentities($results['id_mobil']); ?>"><img src="admin/img/vehicleimages/<?php echo htmlentities($results['image1']); ?>" class="img-responsive" alt="image"></a>
+                      <ul>
+                        <li><i class="fa fa-car" aria-hidden="true"></i><?php echo htmlentities($results['bb']); ?></li>
+                        <li><i class="fa fa-calendar" aria-hidden="true"></i><?php echo htmlentities($results['tahun']); ?> Model</li>
+                        <li><i class="fa fa-user" aria-hidden="true"></i><?php echo htmlentities($results['seating']); ?> Seats</li>
+                      </ul>
+                    </div>
+                    <div class="car-title-m">
+                      <h6><a href="vehical-details.php?vhid=<?php echo htmlentities($results['id_mobil']); ?>"><?php echo htmlentities($results['nama_merek']); ?> , <?php echo htmlentities($results['nama_mobil']); ?></a></h6>
+                    </div>
+                    <div class="inventory_info_m">
+                      <p><?php echo substr($results['deskripsi'], 0, 70); ?></p>
+                    </div>
+                  </div>
+                </div>
+            <?php }
+            } ?>
+
+          </div>
+        </div>
+      </div>
+  </section>
+  <!-- /Resent Cat -->
+
+
+  <!--Footer -->
+  <?php include('includes/footer.php'); ?>
+  <!-- /Footer-->
+
+  <!--Back to top-->
+  <div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+  <!--/Back to top-->
+
+  <!--Login-Form -->
+  <?php include('includes/login.php'); ?>
+  <!--/Login-Form -->
+
+  <!--Register-Form -->
+  <?php include('includes/registration.php'); ?>
+
+  <!--/Register-Form -->
+
+  <!--Forgot-password-Form -->
+  <?php include('includes/forgotpassword.php'); ?>
+  <!--/Forgot-password-Form -->
+
+  <!-- Scripts -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/interface.js"></script>
+  <!--Switcher-->
+  <!-- <script src="assets/switcher/js/switcher.js"></script> -->
+  <!--bootstrap-slider-JS-->
+  <script src="assets/js/bootstrap-slider.min.js"></script>
+  <!--Slider-JS-->
+  <script src="assets/js/slick.min.js"></script>
+  <script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+<!-- Mirrored from themes.webmasterdriver.net/carforyou/demo/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Jun 2017 07:22:11 GMT -->
+
+</html>
---
+++ vehical-details.php
@@ -0,0 +1,358 @@
+<?php
+session_start();
+include('includes/config.php');
+// include('includes/format_rupiah.php');
+error_reporting(0);
+?>
+
+
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <title>Kendaraan Dinas Kolinlamil</title>
+  <!--Bootstrap -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+  <!--Custome Style -->
+  <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+  <!--OWL Carousel slider-->
+  <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+  <!--slick-slider -->
+  <link href="assets/css/slick.css" rel="stylesheet">
+  <!--bootstrap-slider -->
+  <link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+  <!--FontAwesome Font Style -->
+  <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+  <!-- SWITCHER -->
+  <link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
+  <link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
+  <link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
+  <link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
+  <link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
+  <link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
+  <link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+  <link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+</head>
+
+<body>
+
+  <!--Header-->
+  <?php include('includes/header.php'); ?>
+  <!-- /Header -->
+
+  <!--Listing-Image-Slider-->
+
+  <?php
+  $vhid = intval($_GET['vhid']);
+  $sql = "SELECT mobil.*, merek.*  from mobil, merek WHERE merek.id_merek=mobil.id_merek AND mobil.id_mobil='$vhid'";
+  $query = mysqli_query($koneksidb, $sql);
+  if (mysqli_num_rows($query) > 0) {
+    while ($result = mysqli_fetch_array($query)) {
+      $_SESSION['brndid'] = $result['id_merek'];
+  ?>
+
+      <section id="listing_img_slider">
+        <div><img src="admin/img/vehicleimages/<?php echo htmlentities($result['image1']); ?>" class="img-responsive" alt="image" width="900" height="560"></div>
+        <div><img src="admin/img/vehicleimages/<?php echo htmlentities($result['image2']); ?>" class="img-responsive" alt="image" width="900" height="560"></div>
+        <div><img src="admin/img/vehicleimages/<?php echo htmlentities($result['image3']); ?>" class="img-responsive" alt="image" width="900" height="560"></div>
+        <div><img src="admin/img/vehicleimages/<?php echo htmlentities($result['image4']); ?>" class="img-responsive" alt="image" width="900" height="560"></div>
+        <?php if ($result['image5'] == "") {
+        } else {
+        ?>
+          <div><img src="admin/img/vehicleimages/<?php echo htmlentities($result['image5']); ?>" class="img-responsive" alt="image" width="900" height="560"></div>
+        <?php } ?>
+      </section>
+      <!--/Listing-Image-Slider-->
+
+
+      <!--Listing-detail-->
+      <section class="listing-detail">
+        <div class="container">
+
+          <div class="row">
+            <div class="col-md-9">
+              <div class="main_features">
+                <ul>
+
+                  <li> <i class="fa fa-calendar" aria-hidden="true"></i>
+                    <h5><?php echo htmlentities($result['tahun']); ?></h5>
+                    <p>Tahun Registrasi</p>
+                  </li>
+                  <li> <i class="fa fa-cogs" aria-hidden="true"></i>
+                    <h5><?php echo htmlentities($result['bb']); ?></h5>
+                    <p>Tipe Bahan Bakar</p>
+                  </li>
+
+                  <li> <i class="fa fa-user-plus" aria-hidden="true"></i>
+                    <h5><?php echo htmlentities($result['seating']); ?></h5>
+                    <p>Seats</p>
+                  </li>
+                </ul>
+              </div>
+              <div class="listing_more_info">
+                <div class="listing_detail_wrap">
+                  <!-- Nav tabs -->
+                  <ul class="nav nav-tabs gray-bg" role="tablist">
+                    <li role="presentation"><a href="#vehicle-overview " aria-controls="vehicle-overview" role="tab" data-toggle="tab">Deskripisi Kendaraan</a></li>
+
+                    <li role="presentation"><a href="#accessories" aria-controls="accessories" role="tab" data-toggle="tab">Accessories</a></li>
+
+                    <li role="presentation"><a href="#detail" aria-controls="detail" role="tab" data-toggle="tab">Detail Kendaraan</a></li>
+
+
+                    <!-- <li role="presentation" class="active"><a href="#accessories" aria-controls="accessories" role="tab" data-toggle="tab">Kondisi</a></li> -->
+                  </ul>
+
+                  <!-- Tab panes -->
+                  <div class="tab-content">
+                    <!-- vehicle-overview -->
+                    <div role="tabpanel" class="tab-pane active" id="vehicle-overview">
+
+                      <p><?php echo htmlentities($result['deskripsi']); ?></p>
+                      <p>nomor randis = <?php echo htmlentities($result['nopol']); ?></p>
+                    </div>
+
+                    <!-- Detail Kendaraan -->
+                    <div role="tabpanel" class="tab-pane" id="detail">
+                      <!--detail  -->
+                      <table>
+                        <thead>
+                          <tr>
+                            <th colspan="3">Detail</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>Satker</td>
+                            <td><?php echo htmlentities($result['satker']); ?></td>
+                          </tr>
+                          <tr>
+                            <td>Pemilik Kendaraan</td>
+                            <td><?php echo htmlentities($result['pemegang']); ?></td>
+                          </tr>
+                          <tr>
+                            <td>Jenis Kendaraan</td>
+                            <td><?php echo htmlentities($result['jenis_kendaraan']); ?></td>
+                          </tr>
+                          <tr>
+                            <td>Nama Kendaraan</td>
+                            <td><?php echo htmlentities($result['nama_kendaraan']); ?></td>
+                          </tr>
+                          <tr>
+                            <td>Nomor Plat AL Pusat</td>
+                            <td><?php echo htmlentities($result['nomor_al_pusat']); ?></td>
+                          </tr>
+                          <tr>
+                            <td>Nomor Plat AL Kotama</td>
+                            <td><?php echo htmlentities($result['nomor_al_kotama']); ?></td>
+                          </tr>
+                          <tr>
+                            <td>Nomor Mesin</td>
+                            <td><?php echo htmlentities($result['nomor_mesin']); ?></td>
+                          </tr>
+                          <tr>
+                            <td>Nomor Rangka</td>
+                            <td><?php echo htmlentities($result['nomor_rangka']); ?></td>
+                          </tr>
+                          <tr>
+                            <td>Perolehan</td>
+                            <td><?php echo htmlentities($result['perolehan']); ?></td>
+                          </tr>
+                          <tr>
+                            <td>Kondisi Kendaraan</td>
+                            <td><?php echo htmlentities($result['kondisi']); ?></td>
+                          </tr>
+
+
+                        </tbody>
+                      </table>
+                    </div>
+                    <!-- Accessories -->
+                    <div role="tabpanel" class="tab-pane" id="accessories">
+                      <!--Accessories-->
+                      <table>
+                        <thead>
+                          <tr>
+                            <th colspan="2">Accessories</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>Air Conditioner</td>
+                            <?php if ($result['AirConditioner'] == 1) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+
+                          <tr>
+                            <td>AntiLock Braking System</td>
+                            <?php if ($result['AntiLockBrakingSystem==1']) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+
+                          <tr>
+                            <td>Power Steering</td>
+                            <?php if ($result['PowerSteering'] == 1) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+
+
+                          <tr>
+
+                            <td>Power Windows</td>
+
+                            <?php if ($result['PowerWindows'] == 1) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+
+                          <tr>
+                            <td>CD Player</td>
+                            <?php if ($result['CDPlayer'] == 1) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+
+                          <tr>
+                            <td>Leather Seats</td>
+                            <?php if ($result['LeatherSeats'] == 1) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+
+                          <tr>
+                            <td>Central Locking</td>
+                            <?php if ($result['CentralLocking==1']) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+
+                          <tr>
+                            <td>Power Door Locks</td>
+                            <?php if ($result['PowerDoorLocks'] == 1) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+                          <tr>
+                            <td>Brake Assist</td>
+                            <?php if ($result['BrakeAssist'] == 1) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php  } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+
+                          <tr>
+                            <td>Driver Airbag</td>
+                            <?php if ($result['DriverAirbag'] == 1) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+
+                          <tr>
+                            <td>Passenger Airbag</td>
+                            <?php if ($result['PassengerAirbag'] == 1) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+
+                          <tr>
+                            <td>Crash Sensor</td>
+                            <?php if ($result['CrashSensor'] == 1) {
+                            ?>
+                              <td><i class="fa fa-check" aria-hidden="true"></i></td>
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
+                          </tr>
+
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+          <?php }
+      } ?>
+
+
+      </section>
+      <!--/Listing-detail-->
+
+      <!--Footer -->
+      <?php include('includes/footer.php'); ?>
+      <!-- /Footer-->
+
+      <!--Back to top-->
+      <div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+      <!--/Back to top-->
+
+      <!--Login-Form -->
+      <?php include('includes/login.php'); ?>
+      <!--/Login-Form -->
+
+      <!--Register-Form -->
+      <?php include('includes/registration.php'); ?>
+
+      <!--/Register-Form -->
+
+      <!--Forgot-password-Form -->
+      <?php include('includes/forgotpassword.php'); ?>
+
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/bootstrap.min.js"></script>
+      <script src="assets/js/interface.js"></script>
+      <script src="assets/switcher/js/switcher.js"></script>
+      <script src="assets/js/bootstrap-slider.min.js"></script>
+      <script src="assets/js/slick.min.js"></script>
+      <script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+</html>
